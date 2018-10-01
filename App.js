e6f259a5861bcc8053ad47cfcd5396e8ce8faffc
Akash <akash.mindinventory@gmail.com>
2018-10-01 16:28:21 +0530

    init

---
+++ App.js
@@ -0,0 +1,48 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        height: 400,
+        width: 400,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+});
+
+export default class App extends Component {
+    render() {
+        const { region } = this.props;
+
+        return (
+            <View style ={styles.container}>
+                <MapView
+                    provider={PROVIDER_GOOGLE}
+                    style={styles.map}
+                    region={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.015,
+                        longitudeDelta: 0.0121,
+                    }}
+                ></MapView>
+            </View>
+        );
+    }
+}
